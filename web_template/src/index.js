6e9712e3e6956dfbed061f052169a30b1c2c6013
MoonNek0 <wonghoming111105@gmail.com>
2020-05-06 23:15:40 +0800

    add web temp

---
+++ web_template/src/index.js
@@ -0,0 +1,26 @@
+$.getJSON("data.json", function (data) {
+  var items = [];
+  var itemsKey = "";
+  var prices = [];
+  var pricesKey = "";
+  $.each(data, function (key, val) {
+    if (key === "Title") {
+      itemsKey = key;
+      items = val;
+    } else if (key === "Price") {
+      pricesKey = key;
+      prices = val;
+    }
+  });
+  console.log(items)
+  for (var i = 0; i < items.length; i++) {
+    var htmlStr = '<div class="col mb-4">'
+    htmlStr += '<div class="card">'
+    htmlStr += '<div class="card-body">'
+    htmlStr += '<h5 class="card-title">' + items[i] + '</h5>'
+    htmlStr += '<p class="card-text">' + prices[i] + '</p>'
+    htmlStr += '</div></div></div>'
+    $("#cardbodycontent").append(htmlStr);
+  }
+});
+
