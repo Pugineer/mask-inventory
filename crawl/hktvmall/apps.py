bb0a9a795ddf8ce10264380ecb30c7b73d6cf8fe
Peachrara <63202938+Peachrara@users.noreply.github.com>
2020-05-06 12:38:26 +0800

    testing

---
+++ crawl/hktvmall/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class HktvmallConfig(AppConfig):
+    name = 'hktvmall'
---
+++ crawl/hktvmall/views.py
@@ -0,0 +1,13 @@
+import os
+
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+from django.views.static import serve
+
+
+def index(request):
+    filepath = os.getcwd()
+    filepath += "\hktv.json"
+    return serve(request, os.path.basename(filepath), os.path.dirname(filepath))
