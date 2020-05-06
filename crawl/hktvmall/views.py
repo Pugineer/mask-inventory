e0a662c5b455b98b5578cb9f356d3a01770c2db3
Chris <chrislau828@gmail.com>
2020-05-06 17:39:53 +0800

    test

--- crawl/hktvmall/views.py
+++ crawl/hktvmall/views.py
@@ -3,11 +3,17 @@ import os
 # Create your views here.
 from django.views.static import serve
 from hktv import crawlHKTV
+from django.shortcuts import render
 
 
 def index(request):
+
+    return render(request, 'hktvmall/index.html')
+
+def result(request):
     filepath = os.getcwd() + "/hktv.json"
     if not os.path.isfile(filepath):
         crawlHKTV()
 
     return serve(request, os.path.basename(filepath), os.path.dirname(filepath))
+
