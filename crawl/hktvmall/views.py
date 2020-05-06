749b352150d9bf948eac723b5b9d0c231990cd54
Peachrara <63202938+Peachrara@users.noreply.github.com>
2020-05-07 00:18:46 +0800

    testing

--- crawl/hktvmall/views.py
+++ crawl/hktvmall/views.py
@@ -1,19 +1,18 @@
 import os
 
 # Create your views here.
+from django.http import HttpResponse
 from django.views.static import serve
 from hktv import crawlHKTV
 from django.shortcuts import render
 from rq import Queue
 from worker import conn
 q = Queue(connection=conn)
-
+filepath = os.getcwd() + "/hktv.json"
+if not os.path.isfile(filepath):
+    q.enqueue(crawlHKTV())
 
 def index(request):
-    return render(request, 'hktvmall/index.html')
-
-def result(request):
-    filepath = os.getcwd() + "/hktv.json"
     if not os.path.isfile(filepath):
         q.enqueue(crawlHKTV())
     return serve(request, os.path.basename(filepath), os.path.dirname(filepath))
