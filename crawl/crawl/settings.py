e2e5f97341cbdf2feaf30b756bf64753760b662a
Chris <chrislau828@gmail.com>
2020-05-07 14:59:37 +0800

    beta

--- crawl/crawl/settings.py
+++ crawl/crawl/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '#!^y**ykr2-)17i&l=_-5ou=u0)l#71&0_89$%ta*+txk1t4*h'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1']
 
 
 # Application definition
