9a7d4941c1e3a5ec6d25ddae73e77f3ff96be591
Peachrara <63202938+Peachrara@users.noreply.github.com>
2020-05-07 23:35:50 +0800

    beta

--- crawl/hktvmall/views.py
+++ crawl/hktvmall/views.py
@@ -10,3 +10,8 @@ import os
 
 def index(request):
     return render(request, 'hktvmall/index.html')
+
+def hktv(request):
+    with open(os.getcwd() + '/crawl/hktvmall/templates/hktvmall/hktv.json', encoding="utf-8") as json_data:
+        d = json.load(json_data)
+    return JsonResponse(d, safe=False)
