0268128242c5ee7805ddf4596724b62066ab5c38
Chris <chrislau828@gmail.com>
2020-05-07 14:02:23 +0800

    beta

--- crawl/hktvmall/apps.py
+++ crawl/hktvmall/apps.py
@@ -1,9 +1,11 @@
 from django.apps import AppConfig
-from apscheduler.schedulers.background import BackgroundScheduler
-from hktv import crawlHKTV
-from datetime import datetime
-import os
 
 
 class HktvmallConfig(AppConfig):
     name = 'hktvmall'
+
+    def ready(self):
+        from apscheduler.schedulers.background import BackgroundScheduler
+        from hktv import crawlHKTV
+        from datetime import datetime
+        crawlHKTV()
