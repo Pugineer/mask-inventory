81432e8f3ffae2db2e2e5a8027b59dac38d6ede8
Chris <chrislau828@gmail.com>
2020-05-07 12:17:27 +0800

    beta

--- crawl/hktvmall/views.py
+++ crawl/hktvmall/views.py
@@ -9,8 +9,6 @@ from datetime import datetime
 
 filepath = os.getcwd() + "/hktv.json"
 sched = BackgroundScheduler()
-q = Queue(connection=conn)
-
 
 @sched.scheduled_job('interval', minutes=3, next_run_time=datetime.now())
 def timed_job():
