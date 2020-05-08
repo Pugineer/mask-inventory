3752e6349e4a161b6cbf261a27441179cd06c4e7
Peachrara <63202938+Peachrara@users.noreply.github.com>
2020-05-08 23:27:16 +0800

    beta

--- crawl/watsons.py
+++ crawl/watsons.py
@@ -22,7 +22,7 @@ def crawlWatsons():
     #print("Booting with: " + user_agent)
     options = Options()
     options.binary_location = GOOGLE_CHROME_PATH
-    options.add_argument("--lang=zh-hant");
+    options.add_argument("--lang=zh-TW");
     #options.add_argument(f'user-agent={user_agent}')
     options.add_argument("--headless")
     # options.add_argument('--disable-gpu')
