e504d1c7f84cf6dbdf80faf5aa7731f0302b6290
Peachrara <63202938+Peachrara@users.noreply.github.com>
2020-05-09 00:15:17 +0800

    HKTV and Watsons support

--- crawl/maskInventory/apps.py
+++ crawl/maskInventory/apps.py
@@ -11,13 +11,14 @@ class maskInventoryConfig(AppConfig):
     def ready(self):
         sched = BackgroundScheduler()
 
+        @sched.scheduled_job('interval', minutes=30, next_run_time=datetime.now())
         def HKTV():
             print('This job is run every three minutes.')
             crawlHKTV()
 
+        @sched.scheduled_job('interval', minutes=30, next_run_time=datetime.now())
         def Watsons():
             print('This job is run every three minutes.')
             crawlWatsons()
-
 
         sched.start()
