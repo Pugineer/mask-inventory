38c5175fd545f3439f2643744a55c0cc69f961f2
Chris <chrislau828@gmail.com>
2020-05-28 11:03:49 +0800

    Security problem fixed

--- S3Upload.py
+++ S3Upload.py
@@ -14,12 +14,9 @@ def upload_file(file_name, object_name=None):
     else:
         bucket = os.environ.get('S3_BUCKET')
     # Upload the file
-    #s3_client = boto3.client('s3')
-    ACCESS_ID = "AKIAIZNQVXK6HDFI5IXQ"
-    ACCESS_KEY = "GW9qEFAim5mL5N+8fWe5G3E+Nw4lG6TkAg/Dv7QZ"
     s3_client = boto3.client('s3',
-                             aws_access_key_id=ACCESS_ID,
-                             aws_secret_access_key=ACCESS_KEY)
+                             aws_access_key_id=os.environ['AWS_ACCESS_KEY_ID'],
+                             aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY'])
     try:
         response = s3_client.upload_file(file_name, bucket, object_name)
     except ClientError as e:
