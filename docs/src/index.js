12f63f16bf7a8c9981bbf849ce336a85ce8f257e
Peachrara <63202938+Peachrara@users.noreply.github.com>
2020-06-08 01:00:10 +0800

    beta

--- docs/src/index.js
+++ docs/src/index.js
@@ -40,43 +40,13 @@ $(document).ready(function () {
 
 })
 
-function changeItemType(type){
-    if (type == "mask") {
-        switch (storeData[1]) {
-            case "HKTVMall":
-                storeData[0] = 'https://peachrara.s3-ap-northeast-1.amazonaws.com/mask-inventory/HKTVMall.json';
-                break;
-            case "Watsons":
-                storeData[0] = 'https://peachrara.s3-ap-northeast-1.amazonaws.com/mask-inventory/watsons.json';
-                break;
-            case "Amazon":
-                storeData[0] = 'https://peachrara.s3-ap-northeast-1.amazonaws.com/mask-inventory/amazon.json';
-                break;
-        }
-    }
-
-    if (type == "pig") {
-        switch (storeData[1]) {
-            case "HKTVMall":
-                storeData[0] = 'https://peachrara.s3-ap-northeast-1.amazonaws.com/mask-inventory/HKTVMallPig.json';
-                break;
-            case "Amazon":
-                storeData[0] = 'https://peachrara.s3-ap-northeast-1.amazonaws.com/mask-inventory/amazon.json';
-                break;
-        }
-    }
-
-    $('#ajaxTable').DataTable().clear() //must need
-    $('#ajaxTable').DataTable().ajax.url(storeData[0]).load();//must need
-    $('#ajaxTable').DataTable().ajax.reload()//must need
-}
 function changeHandler(name) {
     let storeData = changeStore(name)
     $('#ajaxTable').DataTable().clear() //must need
     $('#ajaxTable').DataTable().ajax.url(storeData[0]).load();//must need
     $('#ajaxTable').DataTable().ajax.reload()//must need
     $('#storeName').text(storeData[1]);
-    $("#btnGroup").button('reset')
+    $("btn2").removeClass('active');
 };
 
 
@@ -86,12 +56,20 @@ function changeStore(name){
             storeData[0] = 'https://peachrara.s3-ap-northeast-1.amazonaws.com/mask-inventory/HKTVMall.json';
             storeData[1] = "HKTVMall";
             break;
+        case "HKTVMallPig":
+            storeData[0] = 'https://peachrara.s3-ap-northeast-1.amazonaws.com/mask-inventory/HKTVMallPig.json';
+            storeData[1] = "HKTVMall";
+                    break;
         case "Watsons":
             storeData[0] = 'https://peachrara.s3-ap-northeast-1.amazonaws.com/mask-inventory/watsons.json';
             storeData[1] = "Watsons";
             break;
         case "Amazon":
             storeData[0] = 'https://peachrara.s3-ap-northeast-1.amazonaws.com/mask-inventory/amazon.json';
+            storeData[1] = "Amazon";
+            break;
+         case "AmazonPig":
+            storeData[0] = 'https://peachrara.s3-ap-northeast-1.amazonaws.com/mask-inventory/amazonPig.json';
             storeData[1] = "Amazon";
             break;
     }
