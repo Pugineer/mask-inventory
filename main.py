62198fa193960adba904969755460bd18e816dbe
Chris <chrislau828@gmail.com>
2020-06-09 10:33:57 +0800

    feat: Multiprocessing driver

--- main.py
+++ main.py
@@ -7,40 +7,32 @@ from hktv import crawlHKTV, crawlHKTVPig
 from amazon import crawlAmazon
 from apscheduler.schedulers.blocking import BlockingScheduler
 
-
-import multiprocessing
-
-ret = {'foo': False}
-
-def worker(queue):
-    ret = queue.get()
-    ret['foo'] = True
-    queue.put(ret)
+import multiprocessing as mp
 
 if __name__ == '__main__':
-    queue = multiprocessing.Queue()
-    queue.put(ret)
-    p = multiprocessing.Process(target=worker, args=(queue,))
-    p.start()
-    print(queue.get())  # Prints {"foo": True}
-    p.join()
-    # p3 = Process(target=crawlHKTV, args=(10, q))
-    # p3.start()
-    # p4 = Process(target=crawlHKTV, args=(15, q))
-    # p4.start()
-    # p5 = Process(target=crawlHKTV, args=(20, q))
-    # p5.start()
-    # p6 = Process(target=crawlHKTV, args=(25, q))
-    # p6.start()
-    # p7 = Process(target=crawlHKTV, args=(30, q))
-    # p7.start()
-    # p1.join()
-    # p2.join()
-    # p3.join()
-    # p4.join()
-    # p5.join()
-    # p6.join()
-    # p7.join()
-    # print(q.get())
-    # with open(os.getcwd() + '/json/HKTVMall.json', 'w', encoding="utf-8") as outfile:
-    #     json.dump(q.get(), outfile, ensure_ascii=False)
+    mp.set_start_method('spawn')
+    q = mp.Queue()
+    p1 = mp.Process(target=crawlHKTV, args=(0, q))
+    p1.start()
+    p2 = mp.Process(target=crawlHKTV, args=(5, q))
+    p2.start()
+    p3 = Process(target=crawlHKTV, args=(10, q))
+    p3.start()
+    p4 = Process(target=crawlHKTV, args=(15, q))
+    p4.start()
+    p5 = Process(target=crawlHKTV, args=(20, q))
+    p5.start()
+    p6 = Process(target=crawlHKTV, args=(25, q))
+    p6.start()
+    p7 = Process(target=crawlHKTV, args=(30, q))
+    p7.start()
+    p1.join()
+    p2.join()
+    p3.join()
+    p4.join()
+    p5.join()
+    p6.join()
+    p7.join()
+    print(q.get())
+    with open(os.getcwd() + '/json/HKTVMall.json', 'w', encoding="utf-8") as outfile:
+        json.dump(q.get(), outfile, ensure_ascii=False)
