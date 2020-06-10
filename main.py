eb8c4bb5408c027f5191c5d71be9fa85e4b6951c
Peachrara <63202938+Peachrara@users.noreply.github.com>
2020-06-10 17:21:46 +0800

    beta

--- main.py
+++ main.py
@@ -12,25 +12,29 @@ from itertools import chain
 
 import multiprocessing as mp
 
-if __name__ == '__main__':
-    jsonDict = []
-    mp.set_start_method('spawn')
-    manager = mp.Manager()
-    q = manager.Queue()
-    length = 0
-    testData = [0,5,10,15,20,25,30]
-    pool = mp.Pool()
-    result = pool.map_async(crawlHKTV, testData)
-    pool.apply_async(crawlAmazon)
-    pool.apply_async(crawlAmazonPig)
-    pool.apply_async(crawlWatsons)
-    pool.apply_async(crawlHKTVPig)
-    jsonDict = list(chain(*result.get()))
-    with open(os.getcwd() + '/json/HKTVMall.json', 'w', encoding="utf-8") as outfile:
-        json.dump(jsonDict, outfile, ensure_ascii=False, indent=2)
 
-    pool.close()
-    pool.join()
+if __name__ == "__main__":
+    def main():
+        jsonDict = []
+        mp.set_start_method('spawn')
+        manager = mp.Manager()
+        q = manager.Queue()
+        length = 0
+        testData = [0, 5, 10, 15, 20, 25, 30]
+        pool = mp.Pool()
+        result = pool.map_async(crawlHKTV, testData)
+        pool.apply_async(crawlAmazon)
+        pool.apply_async(crawlAmazonPig)
+        pool.apply_async(crawlWatsons)
+        pool.apply_async(crawlHKTVPig)
+        jsonDict = list(chain(*result.get()))
+        with open(os.getcwd() + '/json/HKTVMall.json', 'w', encoding="utf-8") as outfile:
+            json.dump(jsonDict, outfile, ensure_ascii=False, indent=2)
 
+        pool.close()
+        pool.join()
+        upload_file(os.getcwd() + '/json/HKTVMall.json', "mask-inventory/HKTVMall.json")
 
-    upload_file(os.getcwd() + '/json/HKTVMall.json', "mask-inventory/HKTVMall.json")
+    sched = BlockingScheduler()
+    sched.add_job(main, 'interval', hours=12, next_run_time=datetime.datetime.now())
+    sched.start()
